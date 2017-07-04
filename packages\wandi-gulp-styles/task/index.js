3c16ae7d276c060a9491a2e6ca391294b17c9024
Cyrille Perois <cyrille.perois@wandi.fr>
2017-07-04 17:01:20 +0200

    use prettier

--- packages\wandi-gulp-styles/task/index.js
+++ packages\wandi-gulp-styles/task/index.js
@@ -11,51 +11,55 @@ const plumber = require("gulp-plumber");
 const config = require("./config");
 
 const styles = (params = { production: false }) => {
-    const cfg = Object.assign({}, config, params);
+  const cfg = Object.assign({}, config, params);
 
-    const {
-        src,
-        dest,
-        lint,
-        autoprefixerOptions,
-        pxToRemOptions,
-        cssnanoOptions,
-        sassOptions,
-        production,
-    } = cfg;
+  const {
+    src,
+    dest,
+    lint,
+    autoprefixerOptions,
+    pxToRemOptions,
+    cssnanoOptions,
+    sassOptions,
+    production
+  } = cfg;
 
-    const processors = [
-        autoprefixer(autoprefixerOptions),
-        pxtorem(pxToRemOptions),
-    ].concat(production ? [cssnano(cssnanoOptions)] : []);
+  const processors = [
+    autoprefixer(autoprefixerOptions),
+    pxtorem(pxToRemOptions)
+  ].concat(production ? [cssnano(cssnanoOptions)] : []);
 
-    const task = () => {
-        const stream = gulp.src(src)
-            .pipe(plumber());
+  const task = () => {
+    const stream = gulp.src(src).pipe(plumber());
 
-        if (lint) {
-            stream.pipe(stylelint({
-                syntax: "scss",
-                reporters: [{
-                    formatter: "string",
-                    console: true,
-                }],
-            }));
-        }
+    if (lint) {
+      stream.pipe(
+        stylelint({
+          syntax: "scss",
+          reporters: [
+            {
+              formatter: "string",
+              console: true
+            }
+          ]
+        })
+      );
+    }
 
-        stream.pipe(production ? util.noop() : sourcemaps.init())
-            .pipe(sass(sassOptions).on("error", sass.logError))
-            .pipe(postcss(processors))
-            .pipe(production ? util.noop() : sourcemaps.write())
-            .pipe(gulp.dest(dest));
+    stream
+      .pipe(production ? util.noop() : sourcemaps.init())
+      .pipe(sass(sassOptions).on("error", sass.logError))
+      .pipe(postcss(processors))
+      .pipe(production ? util.noop() : sourcemaps.write())
+      .pipe(gulp.dest(dest));
 
-        return stream;
-    };
+    return stream;
+  };
 
-    task.displayName = "styles";
-    task.description = "Compile Sass / add prefixes to generated CSS";
+  task.displayName = "styles";
+  task.description = "Compile Sass / add prefixes to generated CSS";
 
-    return task;
+  return task;
 };
 
 module.exports = styles;
